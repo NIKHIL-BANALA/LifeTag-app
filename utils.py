121d3862166705874b3eb4a9f75e9661cff79ac2
NIKHIL BANALA <nikhilb123k@gmail.com>
2025-08-15 22:35:44 +0530

    Added email, password to .env variables

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from flask import session, redirect, url_for
-
+import os
 def login_required(role):
     def wrapper(fn):
         @wraps(fn)
@@ -17,15 +17,16 @@ from flask_mail import Mail, Message
 from datetime import datetime, timedelta
 
 mail = Mail()
-
+email = os.environ.get('EMAIL')
+password = os.environ.get('MAIL_PASSWORD')
 def init_mail(app):
     """Initialize Flask-Mail with app config."""
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'nikhilbanala28k@gmail.com'  # Replace
-    app.config['MAIL_PASSWORD'] = 'rzfpzdjropifpmhs'     # Replace
-    app.config['MAIL_DEFAULT_SENDER'] = ('LifeTag OTP', 'nikhilbanala28k@gmail.com')
+    app.config['MAIL_USERNAME'] = email  # Replace
+    app.config['MAIL_PASSWORD'] = password     # Replace
+    app.config['MAIL_DEFAULT_SENDER'] = ('LifeTag OTP', email)
     mail.init_app(app)
 
 def generate_otp(length=6):
